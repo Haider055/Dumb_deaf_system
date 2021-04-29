08dba99518320b0417b8436590c0298319ca25da
haider <haiderzafar055@gmail.com>
2021-04-29 16:24:32 +0500

    almost complete

---
+++ app/src/main/java/com/dumb/dumb_deaf_system/splashscreen2.java
@@ -0,0 +1,14 @@
+package com.dumb.dumb_deaf_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashscreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen2);
+    }
+}
